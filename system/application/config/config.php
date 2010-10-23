e608dfafd052718a0fbd8e0d8763281447dd1061
Sunil Shenoy <sunil@sunilshenoy.com>
2010-10-23 17:12:42 +0530

    Added config and database.php

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://dev.thegigbazaar.com/";
+$config['base_url']	= "http://localhost:8888/gitgigbazaar/The-GIG-bazaar/";
 
 /*
 |--------------------------------------------------------------------------
