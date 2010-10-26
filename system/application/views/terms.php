72882d4ce92f833437991f6ee53422500bb03ea5
Sanat Hegde <h.sanat@gmail.com>
2010-10-26 20:32:31 +0530

    changed terms of service

--- system/application/views/terms.php
+++ system/application/views/terms.php
@@ -7,23 +7,24 @@
   <h2>The GIG Bazaar Terms of Service</h2>
 
              
-              <ul> 
+              
                    <h3>Sellers</h3>
-                    <li>Basic Rules</li>
+                    <h3>Basic Rules</h3>
+<ul>
                     <li>You need to be a Registered The GIG Bazaar user to sell and buy. Registration is free.</li>
                     <li>Services that are offered by sellers on The GIG Bazaar are called &quot;Gigs&quot;.</li>
                     <li>Gig prices are fixed at Rs.500.</li>
                     <li>For each of your gigs that was ordered and delivered, your net share is Rs.400.</li>
                     <li>Sellers and buyers identity is anonymous. Do not request or provide emails to users on the site</li>
-                    <li>Sellers may not accept payments from The GIG Bazaar buyers outside of The GIG Bazaar (directly through PayPal or any other service)</li>
+                    <li>Sellers may not accept payments from The GIG Bazaar buyers outside of The GIG Bazaar (directly through bank account or any other service)</li>
                     <li>Posting of adult, illegal, copyright protected, violent, nonsense or other improper material on the site is strictly prohibited</li>
                     <li>Content on the site is moderated. Users posting, messaging or   suggesting any of the above prohibited subjects will get their account   blocked permanently</li>
                     <h3>Payment Process</h3>
                     <li>When buyers order one of your gigs, they will be required to pay for the gig in advance.</li>
-                    <li>When you deliver the order, The GIG Bazaar will add your net share to your balance.</li>
-                    <li>You can withdraw funds from your balance to your PayPal account (see more details below).</li>
+                    <li>When you deliver the order, The GIG Bazaar will add your net share to your balance at the end of 15 days.</li>
+                    <li>You can withdraw funds from your balance to your bank account (see more details below).</li>
                   <h3>Handling Orders</h3>
-                    <li>When a buyer orders one of your gigs, you will be notified and   asked to Accept or Reject the order. Rejecting an order should only be   used when you will not be able to perform your offering on time. If you   are away and cannot handle orders, it is good idea to suspend your gigs   from your &quot;My Store&quot; control panel.</li>
+                    <li>When a buyer orders one of your gigs, you will be notified and   asked to Accept or Reject the order. Rejecting an order should only be   used when you will not be able to perform your offering on time. <!--If you   are away and cannot handle orders, it is good idea to suspend your gigs   from your &quot;My Store&quot; control panel.--></li>
                     <li>You have 24 hours to decide if you accept the order, after that time the order may get cancelled by The GIG Bazaar or your buyer.</li>
                     <li>Responding to orders quickly will increase your positive feedback.</li>
                     <li>Delivering Your Work, Passing Material and Submitting Completed Work</li>
@@ -31,19 +32,25 @@
                     <li>You are responsible to scan all transferred files for viruses   and malware. The GIG Bazaar will not be held responsible for any damages caused   by infected files.</li>
                     <li>You can access all communications and files transferred related to an order from the Order Details page.</li>
                     <li>Make sure you describe your gig's final deliverable in detail. Indicate amounts, durations, file formats etc.</li>
-                    <li>Seller's Positive Feedback.</li>
+      </ul>
+                    <h3>Seller's Positive Feedback.</h3>
+                    <ul>
                     <li>Your positive feedback is based on buyers feedback and your responsiveness to new gig orders (Accept/Reject).</li>
                     <li>Work quality and buyer feedback have a dramatic effect on your Positive Feedback Score.</li>
-                    <li>Withdrawing Funds</li>
+                    </ul>
+      <h3>Withdrawing Funds</h3>
+                    <ul>
                     <li>To withdraw funds from GIG Bazaar, you need to have a valid bank account.</li>
-                    <li>Funds are cleared for withdrawal after a safety clearing period of 14 days (from the day you receive them in your balance).</li>
+                    <li>Funds withdrawn are passed on for clearing.</li>
+                    <li>The GIG Bazaar staff will transfer all payment only on the 1st and 15th of each month. </li>
                     <li>Withdrawals are final and cannot be undone. Please make sure to   use your valid and current bank account details. We will not be able to   refund or change this process once it has started and moved to a bank's hands.</li>
-                 
+                 </ul>
                
                   <h3>Buyers</h3>
                   
-                    <li>Ordering Gigs</li>
-                    <li>Purchase of gigs from The GIG Bazaar can be done using debit card, net banking or credit card (credit card purchases are handled by CCAvenue as well).</li>
+                    <h3>Ordering Gigs</h3>
+                    <ul>
+                    <li>Purchase of gigs from The GIG Bazaar can be done using Visa, mastercard or paypal.</li>
                     <li>Once payment is confirmed, your order will be sent to the seller.</li>
                     <li>You can cancel your order at any time until the seller Accepts   your order. Once the seller accepts your order you will not be able to   cancel it.</li>
                     <li>Sellers have the option to reject an order if they can't handle   it on time. If for any reason your order gets rejected, your Rs.500 will be   placed in your  GIG Bazaar balance and will be available to you for   purchasing other gigs on The GIG Bazaar.</li>
@@ -60,11 +67,11 @@
                    
                       <h3>Disputes </h3>
            
-                    <li>We encourage our buyers and sellers to resolve any issues between themselves. If for any reason this fails, you can <a href="#">contact us here</a>.</li>
+                    <li>We encourage our buyers and sellers to resolve any issues between themselves. If for any reason this fails, you can <a href="<?php echo site_url('main/contact');?>">contact us here</a>.</li>
                     <li>The GIG Bazaar will not refund buyers to bank accounts. Instead,   refunded funds will placed into the buyer's balance and will be made   available for future purchases on The GIG Bazaar.</li>
                 
            
-              </ul>
+</ul>
             </div>
          
 
