601e4562fc79ac0e016922f6a0d5b0a2a07544a1
Sanat Hegde <h.sanat@gmail.com>
2010-10-26 20:01:11 +0530

    added analytics to header

--- system/application/views/header.php
+++ system/application/views/header.php
@@ -17,6 +17,21 @@
 					<link href="<?php echo base_url();?>css/thickbox.css" rel="stylesheet" type=	"text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $title;?></title>
+<!--Google Analytics-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-686180-9']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!--analytics ends-->
 </head>
 <body>
 <div id="wrapper">
